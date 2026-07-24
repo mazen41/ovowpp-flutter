90476fc7dac9e91fa26f92a3b7df8754c57e2a4a
mazen41 <mazehossny@gmail.com>
2026-07-24 15:16:46 +0300

    Change API endpoint to user's server ovowpp.loop-pr.com

--- lib/environment.dart
+++ lib/environment.dart
@@ -17,8 +17,8 @@ class Environment {
   // API END POINT URL
   static const MAIN_API_URL = DEV_MODE ? TEST_API_URL : LIVE_API_URL; // Don't touch here
 
-  static const LIVE_API_URL = 'https://preview.ovosolution.com/ovowpp/demo'; //Live end Point URL
-  static const TEST_API_URL = 'https://preview.ovosolution.com/ovowpp/demo'; //Local or demo or test URL
+  static const LIVE_API_URL = 'https://ovowpp.loop-pr.com'; //Live end Point URL
+  static const TEST_API_URL = 'https://ovowpp.loop-pr.com'; //Local or demo or test URL
 
   static const int maxAudioRecordingSeconds = 20; // 3 minutes
 }
